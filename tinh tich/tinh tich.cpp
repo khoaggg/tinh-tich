3e7ee5642c57c7e619aaf528d966cfa3b6cea24f
ADMIN <ADMIN@LAB1-MAY23>
2024-02-26 08:38:48 +0700

    Add project files.

---
+++ tinh tich/tinh tich.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int so1, so2, tich;
+    cout << "CHUONG TRINH TINH TICH\N";
+    cout << "nhap so thu nhat:";
+    cin >> so1;
+    cout << "nhap so thu hai:";
+    cin >> so2;
+    tich = so1 * so2;
+    cout << so1 << "*" << so2 << "="
+        << tich
+        << endl;
+    return 0;
+}
+
